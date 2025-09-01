16bf5cce2192127084ef4521382d22e6089c2e63
zeryab-afk <zaryabshafiq54@gmail.com>
2025-09-01 12:47:39 +0500

    Use constant for 404 status code

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-const router = require('express').Router();
+const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+
+const router = express.Router();
 
 router.use('/users', require('./users'));
 router.use('/items', require('./clothingItems'));
 
 // Handle non-existent resources
 router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+  res.status(StatusCodes.NOT_FOUND).send({ message: 'Requested resource not found' });
 });
 
 module.exports = router;
